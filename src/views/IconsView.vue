203902d4b18440ddbd49da9a7d277210080a5530
DenisBeauget <azentiaytbpro@gmail.com>
2025-04-22 21:10:37 +0200

    icon path

--- src/views/IconsView.vue
+++ src/views/IconsView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-auto justify-evenly">
     <IconCard
-      link="../assets/icon1.svg"
-      text="Pojox accept any JSON or JSON shema syntax and convert it into most popular programming language"
+      link="@/assets/icon1.svg"
+      text="Pojox accept any JSON or JSON shema syntax and convert it into most popular programming language."
     />
     <IconCard
-      link="../assets/icon2.svg"
+      link="@/assets/icon2.svg"
       text="You dont neet to wait to have your result, Pojox convert element instantly and give you your result in language of your choice."
     />
     <IconCard
-      link="../assets/icon3.svg"
+      link="@/assets/icon3.svg"
       text="In addition to converting, Pojox also ensures that your JSON object or JSON schema is properly formatted."
     />
   </div>
